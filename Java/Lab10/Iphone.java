8feea0a58069bd636bfd9a1ad8b142cec856bf32
Jackson Tweet <jacksontweet@createninnovate.com>
2017-01-30 13:43:08 -0600

    Update Iphone.java

--- Java/Lab10/Iphone.java
+++ Java/Lab10/Iphone.java
@@ -13,7 +13,11 @@ public class iphone extends Applet
     public void paint(Graphics g)
     {
         shape.corner();
+        shape.volume1();
+        shape.volume2();
+        shape.muteringer();        
         
+        detail.iphonetxt();
         detail.iphonetxt();
         detail.applelogo();
     }
