9a88ca64f59d1c465026263fd32445bac2976170
Jackson Tweet <jacksontweet@createninnovate.com>
2017-01-31 13:41:35 -0600

    Update shape.java

--- Java/Lab10/shape.java
+++ Java/Lab10/shape.java
@@ -1,1 +1,33 @@
 
+
+import java.awt.*;
+import java.applet.*;
+
+class shape extends Applet
+{
+
+    public void paint(Graphics g)
+    {
+
+    }
+    
+    public static void corner(Graphics g)
+    {
+        g.drawArc(100,100,100,100,100,100);
+    }
+    
+    public static void volume1(Graphics g)
+    {
+        g.drawString("iphone", 25, 25);
+    }
+    
+    public static void volume2(Graphics g)
+    {
+        g.drawString("iphone", 25, 25);
+    }
+    
+    public static void muteringer(Graphics g)
+    {
+        g.drawString("iphone", 25, 25);
+    }
+}
