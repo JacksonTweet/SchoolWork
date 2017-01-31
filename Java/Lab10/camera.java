3571df536fa28df5a56863797f4020c3cffd40f0
pear <kerchner99@gmail.com>
2017-01-31 13:41:05 -0600

    Update camera.java

--- Java/Lab10/camera.java
+++ Java/Lab10/camera.java
@@ -1,6 +1,13 @@
 import java.awt.*;
 import java.applet.*;
 
-class camera extends Applet
-{
+class camera extends iphone
+{ private int cameratopX;
+  private int cameratopY;
+  private int camerawidth;
+  private int cameralenth;
+  
+  public camera()
+  cameraStart = new Point(
+  
 }
